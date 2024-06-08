72a4afc6a957e23f79665475db2f0fea6bcb2306
Mikhail Arepev <arepmm@gmail.com>
2024-06-08 23:29:50 +0300

    fix

--- chs2t/src/chs2t-step-pneumo-supply.cpp
+++ chs2t/src/chs2t-step-pneumo-supply.cpp
@@ -49,17 +49,23 @@ void CHS2T::stepPneumoSupply(double t, double dt)
 
     // Концевые краны питательной магистрали
     anglecock_fl_fwd->setPipePressure(main_reservoir->getPressure());
-    anglecock_fl_fwd->setHoseFlow(hose_fl_fwd->getFlow());
+    //anglecock_fl_fwd->setControl(keys);
     anglecock_fl_fwd->step(t, dt);
     anglecock_fl_bwd->setPipePressure(main_reservoir->getPressure());
-    anglecock_fl_bwd->setHoseFlow(hose_fl_bwd->getFlow());
+    //anglecock_fl_bwd->setControl(keys);
     anglecock_fl_bwd->step(t, dt);
 
-    // Рукава тормозной питательной магистрали
+    // Рукава питательной магистрали
     hose_fl_fwd->setPressure(anglecock_fl_fwd->getPressureToHose());
     hose_fl_fwd->setFlowCoeff(anglecock_fl_fwd->getFlowCoeff());
+    hose_fl_fwd->setCoord(railway_coord + dir * orient * (length / 2.0 - anglecock_fl_fwd->getShiftCoord()));
+    hose_fl_fwd->setShiftSide(anglecock_fl_fwd->getShiftSide());
+    //hose_fl_fwd->setControl(keys);
     hose_fl_fwd->step(t, dt);
     hose_fl_bwd->setPressure(anglecock_fl_bwd->getPressureToHose());
     hose_fl_bwd->setFlowCoeff(anglecock_fl_bwd->getFlowCoeff());
+    hose_fl_bwd->setCoord(railway_coord - dir * orient * (length / 2.0 - anglecock_fl_bwd->getShiftCoord()));
+    hose_fl_bwd->setShiftSide(anglecock_fl_bwd->getShiftSide());
+    //hose_fl_bwd->setControl(keys);
     hose_fl_bwd->step(t, dt);
 }
