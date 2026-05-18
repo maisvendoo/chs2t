c51deaf0c52b43021271546bb86e9f51140f5069
avilkinnick <avilkin.nick@gmail.com>
2026-05-18 07:42:41 +0300

    Заменил загрузку AutoTrainStop

--- chs2t/src/chs2t-init-safety-device.cpp
+++ chs2t/src/chs2t-init-safety-device.cpp
@@ -7,6 +7,7 @@
 #include    <ALSN-decoder.h>
 #include    <automatic-train-stop.h>
 #include    <speedmap.h>
+#include    <core/load_module.h>
 
 #include    <QDir>
 
@@ -41,7 +42,7 @@ void CHS2T::initSafetyDevices(const QString& modules_dir, const QString& custom_
         speed_meter[cab_idx]->read_config("3SL-2M", custom_cfg_dir);
 
         // ЭПК автостопа
-        epk[cab_idx] = loadAutoTrainStop(modules_dir + QDir::separator() + "epk150");
+        epk[cab_idx] = LOAD_MODULE(AutoTrainStop, modules_dir + QDir::separator() + "epk150");
         epk[cab_idx]->read_config("epk150");
 
         // Дешифратор АЛСН
