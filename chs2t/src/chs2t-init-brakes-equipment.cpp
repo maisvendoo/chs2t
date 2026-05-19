7f140f78ec195974f455e5a8d75664822e588837
avilkinnick <avilkin.nick@gmail.com>
2026-05-19 08:13:21 +0300

    Заменил загрузку ElectroAirDistributor

--- chs2t/src/chs2t-init-brakes-equipment.cpp
+++ chs2t/src/chs2t-init-brakes-equipment.cpp
@@ -25,7 +25,8 @@ void CHS2T::initBrakesEquipment(const QString& modules_dir, const QString& custo
     air_dist->read_config(airdist_config_name);
 
     // Электровоздухораспределитель
-    electro_air_dist = loadElectroAirDistributor(modules_dir + QDir::separator() + electro_airdist_module_name);
+    electro_air_dist = LOAD_MODULE(ElectroAirDistributor,
+        modules_dir + QDir::separator() + electro_airdist_module_name);
     electro_air_dist->read_config(electro_airdist_config_name);
 
     // Запасный резервуар
