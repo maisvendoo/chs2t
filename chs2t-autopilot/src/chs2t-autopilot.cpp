8e381ce74b3e4dcbb5cd4de4454112a640cce38e
Mikhail Arepev <arepmm@gmail.com>
2026-05-22 20:51:37 +0300

    Автопилот обрабатывает запрет тяги от ЭДТ и сматывает позиции до нулевой

--- chs2t-autopilot/src/chs2t-autopilot.cpp
+++ chs2t-autopilot/src/chs2t-autopilot.cpp
@@ -71,7 +71,7 @@ void CHS2TAutopilot::preStep(state_vector_t &Y, double t)
     I_ref = cut(I_ref, 0.0, Imax);
 
     // Блокирование тяги по давлению в ТЦ
-    if (auto_feedback->pBC > 0.04)
+    if ((auto_feedback->pBC > 0.04) || !(auto_feedback->is_allow_traction))
     {
         lock_traction = true;
     }
@@ -102,7 +102,7 @@ void CHS2TAutopilot::preStep(state_vector_t &Y, double t)
     }
 
     // Если превышаем скорость - мотаем вниз до упора
-    if (dv < -dV_traction_off)
+    if ((dv < -dV_traction_off) || !(auto_feedback->is_allow_traction))
     {
         if (auto_feedback->pos != 0)
         {
